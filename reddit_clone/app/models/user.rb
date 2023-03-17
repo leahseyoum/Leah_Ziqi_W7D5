f1b8160b098d2ee71d517d15cbbc66bd8e13c6f5
Leah Seyoum <leahseyoum@gmail.com>
2023-03-17 09:31:25 -0700

    user model

---
+++ reddit_clone/app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+    attr_reader :password
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        bcrypt_obj = BCrypt::Password.new(self.password_digest)
+        bcrypt_obj.is_password?(password)
+    end
+
+    
+end
